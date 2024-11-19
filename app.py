8412828dacf6da5aaaf81b58a61be0c1a6964c57
AlbatrossC <soham_jadhav_aids@moderncoe.edu.in>
2024-11-20 00:42:27 +0530

    Rechange the structure

--- app.py
+++ app.py
@@ -6,29 +6,23 @@ import logging
 app = Flask(__name__)
 logging.basicConfig(level=logging.DEBUG)
 
+
+#Home page
 @app.route('/')
 def home():
     return render_template('index.html')
 
+#FE:Sem1 page
 @app.route('/sem1', methods=['GET', 'POST'])
 def run_sem1():
     app.logger.debug('Accessing /sem1 route')
     return sem1_upload()
 
+#FE:sem2 page 
 @app.route('/fe_sem1_sem2', methods=['GET', 'POST'])
 def run_sem1_sem2():
     app.logger.debug('Accessing /fe_sem1_sem2 route')
     return sem1_sem2_upload()
-
-@app.route('/google10ae92d6aa340b06.html')
-def google_verification():
-    return send_from_directory('static', 'google10ae92d6aa340b06.html')
-
-
-
-@app.route('/sitemap.xml')
-def sitemap():
-    return send_from_directory('static', 'sitemap.xml')
 
 @app.route('/error')
 def error():
